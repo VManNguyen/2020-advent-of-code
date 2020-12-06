8b9d90778aa690d47074158caa2570a39eee0c51
Van Man Nguyen <vanman.nguyen@outlook.fr>
2020-12-06 09:29:21 +0100

    Slightly better dec-06 star 1

--- dec-06/src/main.rs
+++ dec-06/src/main.rs
@@ -1,16 +1,8 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::io::Read;
-
-fn count_answers(group : String) -> i64 {
-    println!("New group: {}", group);
-
-    let mut char_vec : Vec<char> = group.chars().collect();
-    char_vec.sort();
-    char_vec.dedup();
-    
-    char_vec.len() as i64
-}
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 fn main() {
     let file = File::open("input").expect("Failed to read file input");
@@ -19,17 +11,18 @@ fn main() {
     buf_reader.read_to_string(&mut contents)
         .expect("Failed to bufferize file input");
     let lines = contents.lines();
-    let mut count : i64 = 0;
+    let mut count : usize = 0;
 
-    let mut group = String::new();
+    let mut group = HashSet::new();
     for line in lines {
-        group.push_str(line.trim());
+        let h = HashSet::from_iter(line.chars());
+        group = HashSet::from_iter(group.union(&h).cloned());
         if line.is_empty() {
-            count += count_answers(group);
-            group = String::new();
+            count += group.len();
+            group = HashSet::new();
         }
     }
-    count += count_answers(group);
+    count += group.len();
 
     println!("Count: {}", count);
 }
