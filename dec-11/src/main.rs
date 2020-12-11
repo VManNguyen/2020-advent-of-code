fc6dd63a35184cbc153c8e2db3a464e872b0e512
Van Man Nguyen <vanman.nguyen@outlook.fr>
2020-12-11 12:41:50 +0100

    dec-11 star 2

--- dec-11/src/main.rs
+++ dec-11/src/main.rs
@@ -122,6 +122,238 @@ fn simulate_waiting_room(waiting_room : &mut Grid::<char>) {
     }
 
     println!("{} iterations", count);
+    println!("Occupied seats: {}", count_occupied(waiting_room));
+}
+
+fn fov_n(waiting_room : & Grid::<char>, 
+              row : usize, 
+              col : usize) -> u8 {
+    if row == 0 { // There's nothing north
+        return 0;
+    }
+
+    let mut i = row - 1;
+    while i >= 0 {
+        let seat = waiting_room[i][col];
+        if seat == '#' {
+            return 1;
+        } else if seat == 'L' {
+            return 0;
+        }
+        if i == 0 { break; }
+        i -= 1;
+    }
+    0
+}
+
+fn fov_s(waiting_room : & Grid::<char>, 
+              row : usize, 
+              col : usize) -> u8 {
+    if row == waiting_room.size().0 - 1 { // There's nothing south
+        return 0;
+    }
+
+    let mut i = row + 1;
+    while i < waiting_room.size().0 {
+        let seat = waiting_room[i][col];
+        if seat == '#' {
+            return 1;
+        } else if seat == 'L' {
+            return 0;
+        }
+        i += 1;
+    }
+    0
+}
+
+fn fov_w(waiting_room : & Grid::<char>,
+         row : usize,
+         col : usize) -> u8 {
+    if col == 0 { // There's nothing west
+        return 0;
+    }
+
+    let mut i = col - 1;
+    while i >= 0 {
+        let seat = waiting_room[row][i];
+        if seat == '#' {
+            return 1;
+        } else if seat == 'L' {
+            return 0;
+        }
+        if i == 0 { break; }
+        i -= 1;
+    }
+    0
+}
+
+fn fov_e(waiting_room : & Grid::<char>,
+         row : usize,
+         col : usize) -> u8 {
+    if col == waiting_room.size().1 - 1 { // There's nothing east
+        return 0;
+    }
+
+    let mut i = col + 1;
+    while i < waiting_room.size().1 {
+        let seat = waiting_room[row][i];
+        if seat == '#' {
+            return 1;
+        } else if seat == 'L' {
+            return 0;
+        }
+        i += 1;
+    }
+    0
+}
+
+fn fov_nw(waiting_room : & Grid::<char>,
+         row : usize,
+         col : usize) -> u8 {
+    if col == 0 || row == 0 { // There's nothing north-west
+        return 0;
+    }
+
+    let mut i = row - 1;
+    let mut j = col - 1;
+    while i >= 0 && j >= 0 {
+        let seat = waiting_room[i][j];
+        if seat == '#' {
+            return 1;
+        } else if seat == 'L' {
+            return 0;
+        }
+        if i == 0 || j == 0 { break; }
+        i -= 1;
+        j -= 1;
+    }
+    0
+}
+
+fn fov_sw(waiting_room : & Grid::<char>,
+         row : usize,
+         col : usize) -> u8 {
+    if col == 0 || row == waiting_room.size().0 {
+        return 0;
+    }
+
+    let mut i = row + 1;
+    let mut j = col - 1;
+    while i < waiting_room.size().0 && j >= 0 {
+        let seat = waiting_room[i][j];
+        if seat == '#' {
+            return 1;
+        } else if seat == 'L' {
+            return 0;
+        }
+        if j == 0 { break; }
+        i += 1;
+        j -= 1;
+    }
+    0
+}
+
+fn fov_se(waiting_room : & Grid::<char>,
+         row : usize,
+         col : usize) -> u8 {
+    if col == waiting_room.size().1 || row == waiting_room.size().0 {
+        return 0;
+    }
+
+    let mut i = row + 1;
+    let mut j = col + 1;
+    while i < waiting_room.size().0 && j < waiting_room.size().1 {
+        let seat = waiting_room[i][j];
+        if seat == '#' {
+            return 1;
+        } else if seat == 'L' {
+            return 0;
+        }
+        i += 1;
+        j += 1;
+    }
+    0
+}
+
+fn fov_ne(waiting_room : & Grid::<char>,
+         row : usize,
+         col : usize) -> u8 {
+    if col == waiting_room.size().1 || row == 0 {
+        return 0;
+    }
+
+    let mut i = row - 1;
+    let mut j = col + 1;
+    while i >= 0 && j < waiting_room.size().1 {
+        let seat = waiting_room[i][j];
+        if seat == '#' {
+            return 1;
+        } else if seat == 'L' {
+            return 0;
+        }
+        if i == 0 { break; }
+        i -= 1;
+        j += 1;
+    }
+    0
+}
+
+fn seat_state_fov(waiting_room : & Grid::<char>, 
+              row : usize, 
+              col : usize) -> char {
+    let mut occupied_seats : u8 = 0;
+
+    let current_state : char = *(waiting_room.get(row, col).unwrap());
+    if current_state == '.' {
+        return '.';
+    }
+
+    occupied_seats +=
+        fov_n(&waiting_room, row, col) +
+        fov_w(&waiting_room, row, col) +
+        fov_s(&waiting_room, row, col) +
+        fov_e(&waiting_room, row, col) +
+        fov_nw(&waiting_room, row, col) +
+        fov_sw(&waiting_room, row, col) +
+        fov_se(&waiting_room, row, col) +
+        fov_ne(&waiting_room, row, col);
+
+    if current_state == '#' {   // If the seat is occupied
+        if occupied_seats > 4 { // And if 4 adjacent seats are too
+            return 'L';         // Then the dude is leaving his seat
+        }
+    } else {                    // Otherwise, if it is empty
+        if occupied_seats == 0 {// And if nobody is around
+            return '#';         // Then sit there
+        }
+    }
+
+    current_state
+}
+
+
+fn simulate_waiting_room_fov(waiting_room : &mut Grid::<char>) {
+    let mut count : u64 = 0;
+    let mut change : bool = true;
+    let mut waiting_room_next = waiting_room.clone();
+    while change {
+        count += 1;
+        change = false;
+        let size = waiting_room.size();
+        for i in 0..size.0 { // rows
+            for j in 0..size.1 { // cols
+                let next_state = seat_state_fov(waiting_room, i, j);
+                if next_state != waiting_room[i][j] {
+                    waiting_room_next[i][j] = next_state;
+                    change = true;
+                }
+            }
+        }
+        *waiting_room = waiting_room_next.clone();
+    }
+
+    println!("{} iterations", count);
+    println!("Occupied seats: {}", count_occupied(waiting_room));
 }
 
 fn count_occupied(waiting_room : &Grid::<char>) -> u64 {
@@ -149,6 +381,6 @@ fn main() {
     for line in lines {
         waiting_room.push_row(line.chars().collect());
     }
-    simulate_waiting_room(&mut waiting_room);
-    println!("Occupied seats: {}", count_occupied(&waiting_room));
+    simulate_waiting_room(&mut waiting_room.clone());
+    simulate_waiting_room_fov(&mut waiting_room.clone());
 }
